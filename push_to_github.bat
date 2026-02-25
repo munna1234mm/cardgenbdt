0cbec52496b9599f5a19ac45ff6e1aa3649abef9
unknown <munna1234kkfterte@gmail.com>
2026-02-24 12:03:46 -1200

    Add HilltopAds verification TXT file

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
 echo Pushing code to GitHub...
-git push -u origin master
+git push -u https://munna1234mm@github.com/munna1234mm/cardgenbdt.git main
 echo.
 echo If the push was successful, you can close this window.
 echo If it asks for authentication, please sign in.
